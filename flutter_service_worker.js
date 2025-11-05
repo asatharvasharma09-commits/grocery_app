340feb318390d96e71d5e985e41dbef5dea1fc2b
atharva sharma <atharvasharma@atharvas-MacBook-Air.local>
2025-11-05 18:42:27 +0530

    Built web
    Branch: main
    Commit: 8f4eaf360a15c5117db9946f5746e8004fc5e57b
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "64dfb363199d2fd601b2d01d65a241ee",
+const RESOURCES = {"flutter_bootstrap.js": "4ec64e1eafa11b2b8ccff4eb6d0ae963",
 "version.json": "868c6fca6cfa40393c07f0ab75b73b60",
 "index.html": "60431321cdd66b4ae477e8baf9569271",
 "/": "60431321cdd66b4ae477e8baf9569271",
